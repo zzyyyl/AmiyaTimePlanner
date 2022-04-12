10f3e2d47c90a1fa41f8c10488671f4309c6d2e3
zzyyyl <zhengyile@mail.ustc.edu.cn>
2022-04-12 16:41:10 +0800

    perf. 增加一些错误处理并重构代码

--- set.py
+++ set.py
@@ -17,12 +17,12 @@ def accept() -> bool:
 		if choice in ['y', 'n']: break
 	return choice == 'y'
 
-def isTime(s: str) -> bool:
-	time_param = s.split(':')
-	if len(time_param) != 2:
+def isTime(param: str) -> bool:
+	time_params = param.split(':')
+	if len(time_params) != 2:
 		return False
-	hours = time_param[0]
-	minutes = time_param[1]
+	hours = time_params[0]
+	minutes = time_params[1]
 	if not (hours.isdecimal() and minutes.isdecimal()):
 		return False
 	hours = int(hours)
@@ -56,22 +56,25 @@ def dumpConfig():
 
 # Week 0 13:30-15:30 doSomething
 # Week Mon 13:30-15:30 doSomething
-def appendWeekEvent(s: str):
-	assert type(s) == str
-	s = s.split(' ', 1)
-	if s[0].lower() in lower_week:
-		week_index = lower_week.index(s[0].lower())
+def appendWeekEvent(params: str, now, time_schedule):
+	assert "week" in time_schedule
+	week_schedule = time_schedule["week"]
+	assert type(week_schedule) == list
+	assert type(params) == str
+	params = params.split(' ', 1)
+	if params[0].lower() in lower_week:
+		week_index = lower_week.index(params[0].lower())
 	else:
-		week_index = int(s[0])
-	s = s[1].split(' ', 1)
-	if '-' in s[0]:
-		assert len(s[0].split('-')) == 2
-		beginTime, endTime = s[0].split('-')
+		week_index = int(params[0])
+	params = params[1].split(' ', 1)
+	if '-' in params[0]:
+		assert len(params[0].split('-')) == 2
+		beginTime, endTime = params[0].split('-')
 	else:
-		beginTime = s[0]
-		s = s[1].split(' ', 1)
-		endTime = s[0]
-	event = s[1] #allows space
+		beginTime = params[0]
+		params = params[1].split(' ', 1)
+		endTime = params[0]
+	event = params[1] #allows space
 	assert (isTime(beginTime) and isTime(endTime))
 	print(WEEK[week_index], f"{beginTime}-{endTime}, {event}")
 	if accept():
@@ -89,38 +92,41 @@ def appendWeekEvent(s: str):
 # Day thisweek Mon 13:30-15:30 doSomething
 # Day nextweek Mon 13:30-15:30 doSomething
 # Day next1week Mon 13:30-15:30 doSomething
-def appendDayEvent(s: str):
-	assert type(s) == str
-	s = s.split(' ', 1)
-	param = s[0].lower()
+def appendDayEvent(params: str, now, time_schedule):
+	assert "day" in time_schedule
+	day_schedule = time_schedule["day"]
+	assert type(day_schedule) == dict
+	assert type(params) == str
+	params = params.split(' ', 1)
+	param = params[0].lower()
 	if param in lower_day:
 		day_index = lower_day.index(param)
-		day_formatted = datetime.now() + timedelta(days=day_index)
+		day_formatted = now + timedelta(days=day_index)
 	elif param.isdecimal():
 		day_index = int(param)
-		day_formatted = datetime.now() + timedelta(days=day_index)
+		day_formatted = now + timedelta(days=day_index)
 	elif param[:4] == "next" and param[-4:] == "week":
 		if param[4:-4] == '':
 			week_count = 1
 		else:
 			week_count = int(param[4:-4])
-		s = s[1].split(' ', 1)
-		param = s[0].lower()
+		params = params[1].split(' ', 1)
+		param = params[0].lower()
 		if param in lower_week:
 			weekday_index = lower_week.index(param)
 		else:
 			weekday_index = int(param)
-		day_index = week_count * 7 - datetime.now().weekday() + weekday_index
-		day_formatted = datetime.now() + timedelta(days=day_index)
+		day_index = week_count * 7 - now.weekday() + weekday_index
+		day_formatted = now + timedelta(days=day_index)
 	elif param == "thisweek":
-		s = s[1].split(' ', 1)
-		param = s[0].lower()
+		params = params[1].split(' ', 1)
+		param = params[0].lower()
 		if param in lower_week:
 			weekday_index = lower_week.index(param)
 		else:
 			weekday_index = int(param)
-		day_index = weekday_index - datetime.now().weekday()
-		day_formatted = datetime.now() + timedelta(days=day_index)
+		day_index = weekday_index - now.weekday()
+		day_formatted = now + timedelta(days=day_index)
 	else:
 		while True:
 			try: day_formatted = datetime.strptime(param, "%Y-%m-%d")
@@ -129,19 +135,19 @@ def appendDayEvent(s: str):
 			try: day_formatted = datetime.strptime(param, "%y-%m-%d")
 			except: pass
 			else: break
-			try: day_formatted = datetime.strptime(param, "%m-%d").replace(year=datetime.now().year)
+			try: day_formatted = datetime.strptime(param, "%m-%d").replace(year=now.year)
 			except: pass
 			else: break
 			raise ValueError("Unrecognized time data " + param)
-	s = s[1].split(' ', 1)
-	if '-' in s[0]:
-		assert len(s[0].split('-')) == 2
-		beginTime, endTime = s[0].split('-')
+	params = params[1].split(' ', 1)
+	if '-' in params[0]:
+		assert len(params[0].split('-')) == 2
+		beginTime, endTime = params[0].split('-')
 	else:
-		beginTime = s[0]
-		s = s[1].split(' ', 1)
-		endTime = s[0]
-	event = s[1] #allows space
+		beginTime = params[0]
+		params = params[1].split(' ', 1)
+		endTime = params[0]
+	event = params[1] #allows space
 	assert (isTime(beginTime) and isTime(endTime))
 	dateYmd = day_formatted.strftime("%Y-%m-%d")
 	print(dateYmd, f"{beginTime}-{endTime}, {event}")
@@ -156,19 +162,28 @@ def appendDayEvent(s: str):
 		dumpConfig()
 		print("Append success.")
 
-if __name__ == '__main__':
+def appendEvent(input_string, now, time_schedule):
+	scheduleType, params = input_string.split(' ', 1)
+	scheduleType = scheduleType.lower()
+	if scheduleType == "week":
+		appendWeekEvent(params=params, now=now, time_schedule=time_schedule)
+	elif scheduleType == "day":
+		appendDayEvent(params=params, now=now, time_schedule=time_schedule)
+
+def main():
 	time_schedule = loadConfig()
-	week_schedule = time_schedule["week"]
-	day_schedule = time_schedule["day"]
-
 	while True:
-		s = input("Append event:")
-		s = s.split(' ', 1)
+		try:
+			appendEvent(input_string=input("Append event:"),
+						time_schedule=time_schedule,
+						now=datetime.now())
+		except (KeyboardInterrupt, EOFError):
+			break
+		except AssertionError as e:
+			print(e.__repr__())
+		except Exception as e:
+			raise
 
-		if s[0].lower() == 'week':
-			appendWeekEvent(s[1])
-		elif s[0].lower() == 'day':
-			appendDayEvent(s[1])
-
-	s = json.dumps(time_schedule, indent=4)
-	print(s)
+if __name__ == '__main__':
+	main()
+	print("Exit.")
